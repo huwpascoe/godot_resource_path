87f93b1863b4bd7ded7797c4058abca7502a0e5f
yesfish <huwpascoe@users.noreply.github.com>
2024-12-22 12:04:48 +0000

    Support for class_name
    Resources with class_name can now be used like any built-in class.

--- addons/resource_path/editor_inspector_plugin.gd
+++ addons/resource_path/editor_inspector_plugin.gd
@@ -7,9 +7,20 @@ func _can_handle(_object: Object) -> bool:
 	return true
 
 
+func _is_class(name: String):
+	if ClassDB.class_exists(name):
+		return true
+
+	for item in ProjectSettings.get_global_class_list():
+		if item.get("class") == name:
+			return true
+
+	return false
+
+
 func _parse_property(_object: Object, _type: Variant.Type, name: String,
 		hint_type: PropertyHint, hint_string: String, _usage_flags: int, _wide: bool) -> bool:
-	if hint_type == PROPERTY_HINT_FILE and ClassDB.class_exists(hint_string):
+	if hint_type == PROPERTY_HINT_FILE and _is_class(hint_string):
 		add_property_editor(name, ResourcePathEditorProperty.new(hint_string))
 		return true
 	return false
